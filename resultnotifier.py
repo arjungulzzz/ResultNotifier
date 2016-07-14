c721ad7a14634624cedc4c71e329c79eeb781845
Arjun Gulyani <arjungulzzz@gmail.com>
2016-07-14 10:27:16 +0530

    Add files via upload

---
+++ resultnotifier.py
@@ -0,0 +1,38 @@
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import subprocess
+import ctypes
+import requests
+import urllib.request
+import re
+import os
+import time
+MessageBox=ctypes.windll.user32.MessageBoxW
+req = Request("http://duexam.du.ac.in/RSLT_MJ2016/Students/List_Of_Declared_Results.aspx")
+while 1:
+    try:
+        response = urlopen(req)
+    except HTTPError as e:
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+            time.sleep(100)
+    except URLError as e:
+                print('We failed to reach a server.')
+                print('Reason: ', e.reason)
+                time.sleep(100)
+    else:
+        while 1:
+            with urllib.request.urlopen('http://duexam.du.ac.in/RSLT_MJ2016/Students/List_Of_Declared_Results.aspx') as response:
+                html=response.read()
+                matches = html.find(b'39')
+                if matches != -1:
+                    MessageBox(None, 'RESULT NOT OUT YET', 'YEAH!!', 0)
+                    time.sleep(600)
+                else:
+                    subprocess.call(['python','resultsconfirm.py'])
+
+        
+
+
+
+       
